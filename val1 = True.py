b65a0769147a048d1423f409d41965503fa132f0
ayinamubarakk <ayina.mubarak2301051@gmail.com>
2025-11-13 18:24:38 +0500

    Add files via upload

---
+++ val1 = True.py
@@ -0,0 +1,43 @@
+val1 = True
+val2 = False
+
+#and operator
+print("and operator", val1 and val2)
+#or operator
+print("or operator", val1 or val2)
+
+a = 20
+b = 30
+
+print("and operator",(a==b))
+print("or operator",(a==b))
+print("and operator",(a!=b))
+print("or operator",(a!=b))
+
+#type conversions
+a = 10
+b = 12.5
+c = 14.5
+print(a+b+c)
+
+a = 21
+b = 12
+print(int(a + b))
+print(float(a + b))
+print(str(a + b))
+
+a = 25
+print(float(a))
+
+
+# name = input("Enter your name:")
+# print("Welcome" ,name)
+# val = input("Enter some value:")
+# print(type(val), val)
+
+name = input("Enter your name:")
+age = input("Enter your age:")
+marks = input("Enter your marks:")
+print("Wow", marks)
+
+
